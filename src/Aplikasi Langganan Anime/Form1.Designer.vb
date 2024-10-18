8b75f82781d2420988219a2d9543c9aa64b1dc45
Fiqri317 <139039571+Fiqri317@users.noreply.github.com>
2024-10-18 18:16:59 +0700

    main

---
+++ src/Aplikasi Langganan Anime/Form1.Designer.vb
@@ -0,0 +1,433 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.dgv_coba = New System.Windows.Forms.DataGridView()
+        Me.Bs_Coba = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        CType(Me.dgv_coba, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Bs_Coba, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Email"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(1, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Subscription Level"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 243)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(116, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Subscription Start Date"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(13, 152)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Duration"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(13, 274)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(113, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Subscription End Date"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(13, 217)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(31, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Price"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(6, 16)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Status"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(13, 335)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(48, 13)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "Payment"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(145, 62)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(199, 20)
+        Me.TextBox1.TabIndex = 10
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(145, 92)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(199, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(145, 121)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(199, 20)
+        Me.TextBox3.TabIndex = 12
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"7 Days", "30 Days", "6 Months", "1 Years"})
+        Me.ComboBox1.Location = New System.Drawing.Point(145, 149)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(199, 21)
+        Me.ComboBox1.TabIndex = 13
+        Me.ComboBox1.Text = "Pilih"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(132, 11)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(41, 17)
+        Me.RadioButton1.TabIndex = 14
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Pro"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(198, 9)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton2.TabIndex = 15
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Premium"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(145, 214)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(199, 20)
+        Me.TextBox4.TabIndex = 16
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = ""
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(145, 240)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(95, 20)
+        Me.DateTimePicker1.TabIndex = 17
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker2.Location = New System.Drawing.Point(144, 268)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(96, 20)
+        Me.DateTimePicker2.TabIndex = 18
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Gopay", "Dana", "Ovo", "M-Banking"})
+        Me.ComboBox2.Location = New System.Drawing.Point(145, 332)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(199, 21)
+        Me.ComboBox2.TabIndex = 19
+        Me.ComboBox2.Text = "Pilih"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(138, 14)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(55, 17)
+        Me.RadioButton3.TabIndex = 20
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Active"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(209, 14)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(60, 17)
+        Me.RadioButton4.TabIndex = 21
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Expired"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(383, 65)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(78, 13)
+        Me.Label11.TabIndex = 22
+        Me.Label11.Text = "Find Username"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(466, 62)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox5.TabIndex = 23
+        '
+        'dgv_coba
+        '
+        Me.dgv_coba.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_coba.Location = New System.Drawing.Point(386, 92)
+        Me.dgv_coba.Name = "dgv_coba"
+        Me.dgv_coba.Size = New System.Drawing.Size(659, 305)
+        Me.dgv_coba.TabIndex = 24
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 374)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 25
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(111, 374)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Delete"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(205, 374)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 27
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 176)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(332, 32)
+        Me.GroupBox1.TabIndex = 28
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 289)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(338, 37)
+        Me.GroupBox2.TabIndex = 29
+        Me.GroupBox2.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(604, 60)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 30
+        Me.Button4.Text = "Find"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(283, 9)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(477, 37)
+        Me.Label12.TabIndex = 31
+        Me.Label12.Text = "APLIKASI LANGGANAN FILM"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1057, 410)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.dgv_coba)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgv_coba, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Bs_Coba, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents dgv_coba As System.Windows.Forms.DataGridView
+    Friend WithEvents Bs_Coba As System.Windows.Forms.BindingSource
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+
+End Class
---
+++ src/Aplikasi Langganan Anime/Form1.vb
@@ -0,0 +1,160 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.OleDb
+Public Class Form1
+    Dim _koneksiString As String
+    Dim _koneksi As New OleDbConnection
+    Dim komandambil As New OleDbCommand
+    Dim datatabelku As New DataTable
+    Dim dataadapterku As New OleDbDataAdapter
+    Dim x As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        _koneksiString = "Provider=Microsoft.Jet.OleDb.4.0;" + "Data Source=D:\Campus\Semester V\Kecerdasan Komputasi\Aplikasi Langganan Anime\database\Langganan.mdb;"
+        _koneksi.ConnectionString = _koneksiString
+        _koneksi.Open()
+
+        komandambil.Connection = _koneksi
+        komandambil.CommandType = CommandType.Text
+
+        komandambil.CommandText = "SELECT * FROM Pengguna"
+        dataadapterku.SelectCommand = komandambil
+        dataadapterku.Fill(datatabelku)
+        Bs_coba.DataSource = datatabelku
+        dgv_coba.DataSource = Bs_coba
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        priceSubscription()
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        priceSubscription()
+    End Sub
+
+    Private Sub priceSubscription()
+        Dim duration As String = ComboBox1.Text
+        Dim levelSubscription As String = If(RadioButton1.Checked, "Pro", "Premium")
+        Dim price As String = ""
+
+        Select Case duration
+            Case "7 Days"
+                price = If(levelSubscription = "Pro", "20000", "30000")
+            Case "30 Days"
+                price = If(levelSubscription = "Pro", "50000", "100000")
+            Case "6 Months"
+                price = If(levelSubscription = "Pro", "250000", "500000")
+            Case "1 Years"
+                price = If(levelSubscription = "Pro", "1000000", "1500000")
+        End Select
+
+        TextBox4.Text = price
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim cmdTambah As New OleDbCommand
+        Dim tanya As String
+        Dim x As DataRow
+        cmdTambah.Connection = _koneksi
+        cmdTambah.CommandText = "INSERT INTO " + "Pengguna (Username, [Password], Email, Duration, [Subscription Level], Price, [Subscription Start Date], [Subscription End Date], Status, Payment)" +
+            "VALUES ('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + ComboBox1.Text + "','" +
+            If(RadioButton1.Checked, "Pro", "Premium") + "','" + TextBox4.Text + "','" + DateTimePicker1.Text + "','" +
+            DateTimePicker2.Text + "','" + If(RadioButton3.Checked, "Active", "Expired") + "','" + ComboBox2.Text + " ')"
+        tanya = MessageBox.Show("Data Ini di Simpan ?", "info", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If tanya = vbYes Then
+            cmdTambah.ExecuteNonQuery()
+            x = datatabelku.NewRow
+            x("Username") = TextBox1.Text
+            x("Password") = TextBox2.Text
+            x("Email") = TextBox3.Text
+            x("Duration") = ComboBox1.Text
+            x("Subscription Level") = If(RadioButton1.Checked, "Pro", "Premium")
+            x("Price") = TextBox4.Text
+            x("Subscription Start Date") = DateTimePicker1.Text
+            x("Subscription End Date") = DateTimePicker2.Text
+            x("Status") = If(RadioButton3.Checked, "Active", "Expired")
+            x("Payment") = ComboBox2.Text
+            datatabelku.Rows.Add(x)
+            Bs_Coba.DataSource = Nothing
+            Bs_Coba.DataSource = datatabelku
+
+            dgv_coba.Refresh()
+            Bs_Coba.MoveFirst()
+        End If
+    End Sub
+
+    Private Sub dgv_coba_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgv_coba.CellContentClick
+        If dgv_coba.CurrentRow IsNot Nothing Then
+            TextBox1.Text = dgv_coba.CurrentRow.Cells(0).Value.ToString()
+            TextBox2.Text = dgv_coba.CurrentRow.Cells(1).Value.ToString()
+            TextBox3.Text = dgv_coba.CurrentRow.Cells(2).Value.ToString()
+            ComboBox1.Text = dgv_coba.CurrentRow.Cells(3).Value.ToString()
+
+            Dim subscriptionLevel As String = dgv_coba.CurrentRow.Cells(4).Value.ToString()
+            RadioButton1.Checked = (subscriptionLevel = "Pro")
+            RadioButton2.Checked = (subscriptionLevel = "Premium")
+
+            TextBox4.Text = dgv_coba.CurrentRow.Cells(5).Value.ToString()
+            DateTimePicker1.Value = Convert.ToDateTime(dgv_coba.CurrentRow.Cells(6).Value)
+            DateTimePicker2.Value = Convert.ToDateTime(dgv_coba.CurrentRow.Cells(7).Value)
+
+            Dim status As String = dgv_coba.CurrentRow.Cells(8).Value.ToString()
+            RadioButton3.Checked = (status = "Active")
+            RadioButton4.Checked = (status = "Expired")
+
+            ComboBox2.Text = dgv_coba.CurrentRow.Cells(9).Value.ToString()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim cmdHapus As New OleDbCommand
+        cmdHapus.Connection = _koneksi
+        cmdHapus.CommandType = CommandType.Text
+        x = MessageBox.Show("Yakin Data Akan di Hapus ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        Dim username As String = TextBox1.Text.Trim().Replace("'", "''")
+        cmdHapus.CommandText = "DELETE FROM Pengguna WHERE Username = '" & username & "'"
+        cmdHapus.ExecuteNonQuery()
+
+
+        Dim rowToDelete As DataRow = datatabelku.Select("Username='" & username & "'").FirstOrDefault()
+        If rowToDelete IsNot Nothing Then
+            datatabelku.Rows.Remove(rowToDelete)
+        End If
+
+        Bs_Coba.DataSource = Nothing
+        Bs_Coba.DataSource = datatabelku
+        dgv_coba.Refresh()
+
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        ComboBox1.SelectedIndex = -1
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        TextBox4.Clear()
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+        ComboBox2.SelectedIndex = -1
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        datatabelku.Clear()
+        komandambil.Connection = _koneksi
+        komandambil.CommandType = CommandType.Text
+        komandambil.CommandText = "SELECT * FROM " + "Pengguna WHERE Username LIKE '%" + TextBox5.Text + "%'"
+
+        dataadapterku.SelectCommand = komandambil
+        dataadapterku.Fill(datatabelku)
+
+        dgv_coba.Refresh()
+        Bs_Coba.DataSource = datatabelku
+        dgv_coba.DataSource = Bs_Coba
+        Bs_Coba.MoveFirst()
+    End Sub
+End Class
